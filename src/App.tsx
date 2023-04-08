c3b3961e8474ba43f7f7026b11d4ead4910d0135
exslym <76844562+exslym@users.noreply.github.com>
2023-04-08 17:07:39 +0400

    styles added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,36 @@
 import React, { useEffect, useState } from 'react';
-import './App.scss';
+import styles from './Styles.module.scss';
 
 function App() {
+	const [selectedUser, setSelectedUser] = useState<string | null>(null);
+
 	return (
-		<div className='App'>
-			<div className='container'></div>
+		<div className={styles.app}>
+			<div className={styles.container}>
+				<div className={styles.search}>
+					<input type='text' placeholder='search' /> <button>find</button>
+				</div>
+
+				<div className={styles.flexbox}>
+					<ul>
+						{['exslym', 'artem'].map(u => (
+							<li
+								className={selectedUser === u ? styles.selected : ''}
+								onClick={() => {
+									setSelectedUser(u);
+									document.title = u;
+								}}
+							>
+								{u}
+							</li>
+						))}
+					</ul>
+					<div>
+						<h2>Username</h2>
+						<div>Details</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 }
