023151ccdd14aeab3f7560e59095bf8690810b28
exslym <76844562+exslym@users.noreply.github.com>
2023-04-09 00:48:12 +0400

    components created, code refactored

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,135 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import styles from './Styles.module.scss';
 
+//* TYPES:
+type UserType = {
+	login: string;
+	id: number;
+	avatar_url: string;
+	bio: string;
+	public_repos: number;
+	followers: number;
+};
 type SearchUserType = {
 	login: string;
 	id: number;
 };
-type SearchResult = {
+type SearchResultType = {
 	items: SearchUserType[];
+};
+type SearchPropsType = {
+	value: string;
+	onSubmit: (fixedValue: string) => void;
+};
+type UsersListPropsType = {
+	term: string;
+	selectedUser: SearchUserType | null;
+	onUserSelect: (user: SearchUserType) => void;
+};
+type UserDetailsPropsType = {
+	user: SearchUserType | null;
+};
+
+export const Search = (props: SearchPropsType) => {
+	const initialSearchState = 'developer';
+	const [tempSearch, setTempSearch] = useState('');
+	useEffect(() => {
+		setTempSearch(props.value);
+	}, [props.value]);
+
+	return (
+		<>
+			<input
+				type='text'
+				placeholder='search'
+				value={tempSearch}
+				onChange={e => {
+					setTempSearch(e.currentTarget.value);
+				}}
+			/>
+			<button
+				onClick={() => {
+					props.onSubmit(tempSearch);
+				}}
+			>
+				find
+			</button>
+			<button
+				onClick={() => {
+					props.onSubmit(initialSearchState);
+				}}
+			>
+				reset
+			</button>
+		</>
+	);
+};
+
+export const UsersList = (props: UsersListPropsType) => {
+	const [users, setUsers] = useState<SearchUserType[]>([]);
+	useEffect(() => {
+		console.log('SYNC USERS');
+		axios.get<SearchResultType>(`https://api.github.com/search/users?q=${props.term}`).then(res => {
+			setUsers(res.data.items);
+		});
+	}, [props.term]);
+
+	return (
+		<>
+			<ul>
+				{users.map(u => (
+					<li
+						key={u.id}
+						className={props.selectedUser === u ? styles.selected : ''}
+						onClick={() => {
+							props.onUserSelect(u);
+						}}
+					>
+						{u.login}
+					</li>
+				))}
+			</ul>
+		</>
+	);
+};
+
+export const UserDetails = (props: UserDetailsPropsType) => {
+	const [userDetails, setUserDetails] = useState<UserType | null>(null);
+	useEffect(() => {
+		console.log('SYNC USER DETAILS');
+		if (!!props.user) {
+			axios.get<UserType>(`https://api.github.com/users/${props.user.login}`).then(res => {
+				setUserDetails(res.data);
+			});
+		}
+	}, [props.user]);
+
+	return (
+		<>
+			<div className={styles.details}>
+				{userDetails && (
+					<div>
+						<h2 className={styles.username}>{userDetails.login}</h2>
+						<img className={styles.avatar} src={userDetails.avatar_url} alt='' />
+						<div className={styles.bio}>{userDetails.bio}</div>
+						<div className={styles.followers}>
+							followers: <strong>{userDetails.followers}</strong>
+						</div>
+						<div className={styles.repos}>
+							repos: <strong>{userDetails.public_repos}</strong>
+						</div>
+					</div>
+				)}
+			</div>
+		</>
+	);
 };
 
 function App() {
-	const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null);
-	const [users, setUsers] = useState<SearchUserType[]>([]);
-	const [tempSearch, setTempSearch] = useState('');
+	const initialSearchState = 'developer';
 
-	const fetchData = (term: string) => {
-		axios.get<SearchResult>(`https://api.github.com/search/users?q=${term}`).then(res => {
-			setUsers(res.data.items);
-		});
-	};
+	const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null);
+	const [searchTerm, setSearchTerm] = useState(initialSearchState);
 
 	useEffect(() => {
 		console.log('SYNC TITLE');
@@ -28,44 +139,20 @@ function App() {
 		}
 	}, [selectedUser]);
 
-	useEffect(() => {
-		console.log('SYNC USERS');
-		fetchData('webdev');
-	}, []);
-
 	return (
 		<div className={styles.app}>
 			<div className={styles.container}>
 				<div className={styles.search}>
-					<input
-						type='text'
-						placeholder='search'
-						value={tempSearch}
-						onChange={e => {
-							setTempSearch(e.currentTarget.value);
+					<Search
+						value={searchTerm}
+						onSubmit={(value: string) => {
+							setSearchTerm(value);
 						}}
-					/>{' '}
-					<button onClick={() => fetchData(tempSearch)}>find</button>
+					/>
 				</div>
-
 				<div className={styles.flexbox}>
-					<ul>
-						{users.map(u => (
-							<li
-								key={u.id}
-								className={selectedUser === u ? styles.selected : ''}
-								onClick={() => {
-									setSelectedUser(u);
-								}}
-							>
-								{u.login}
-							</li>
-						))}
-					</ul>
-					<div>
-						<h2>Username</h2>
-						<div>Details</div>
-					</div>
+					<UsersList term={searchTerm} selectedUser={selectedUser} onUserSelect={setSelectedUser} />
+					<UserDetails user={selectedUser} />
 				</div>
 			</div>
 		</div>
