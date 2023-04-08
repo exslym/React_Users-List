c830ec8482a93b3df437e0089f6148d27bcec7b2
exslym <76844562+exslym@users.noreply.github.com>
2023-04-08 17:50:33 +0400

    axios added

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,64 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import styles from './Styles.module.scss';
 
+type SearchUserType = {
+	login: string;
+	id: number;
+};
+type SearchResult = {
+	items: SearchUserType[];
+};
+
 function App() {
-	const [selectedUser, setSelectedUser] = useState<string | null>(null);
+	const [selectedUser, setSelectedUser] = useState<SearchUserType | null>(null);
+	const [users, setUsers] = useState<SearchUserType[]>([]);
+	const [tempSearch, setTempSearch] = useState('');
+
+	const fetchData = (term: string) => {
+		axios.get<SearchResult>(`https://api.github.com/search/users?q=${term}`).then(res => {
+			setUsers(res.data.items);
+		});
+	};
+
+	useEffect(() => {
+		console.log('SYNC TITLE');
+		if (selectedUser) {
+			document.title = selectedUser.login;
+		}
+	}, [selectedUser]);
+
+	useEffect(() => {
+		console.log('SYNC USERS');
+		fetchData('webdev');
+	}, []);
 
 	return (
 		<div className={styles.app}>
 			<div className={styles.container}>
 				<div className={styles.search}>
-					<input type='text' placeholder='search' /> <button>find</button>
+					<input
+						type='text'
+						placeholder='search'
+						value={tempSearch}
+						onChange={e => {
+							setTempSearch(e.currentTarget.value);
+						}}
+					/>{' '}
+					<button onClick={() => fetchData(tempSearch)}>find</button>
 				</div>
 
 				<div className={styles.flexbox}>
 					<ul>
-						{['exslym', 'artem'].map(u => (
+						{users.map(u => (
 							<li
+								key={u.id}
 								className={selectedUser === u ? styles.selected : ''}
 								onClick={() => {
 									setSelectedUser(u);
-									document.title = u;
 								}}
 							>
-								{u}
+								{u.login}
 							</li>
 						))}
 					</ul>
